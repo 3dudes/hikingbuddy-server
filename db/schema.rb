6b2056544b23a3b72e383da880bc7c6000b3bba2
Patrick Gruber <gruber_patrick@hotmail.de>
2015-05-16 13:36:11 +0000

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516115843) do
+ActiveRecord::Schema.define(version: 20150516132412) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150516115843) do
     t.decimal  "longitude",  precision: 15, scale: 10
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
