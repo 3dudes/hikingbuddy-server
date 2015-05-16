f738a9ecd82eda87aafc1efaf6755a1caa6c0cc0
Patrick Gruber <gruber_patrick@hotmail.de>
2015-05-16 15:16:35 +0000

    added mission serializer

--- app/controllers/api/missions_controller.rb
+++ app/controllers/api/missions_controller.rb
@@ -1,4 +1,8 @@
 module Api
   class MissionsController < Api::ApplicationController
+    def show
+      mission = Mission.find(params[:id])
+      render json: mission, serializer: MissionSerializer, status: :ok
+    end
   end
 end
