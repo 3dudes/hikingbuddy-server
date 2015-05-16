b7d13c02684486f826227b6f35a527ad7a80607f
Philip Giuliani <giiuliio@gmail.com>
2015-05-16 15:48:18 +0000

    Add #find_by_serial!

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -5,11 +5,18 @@ class Mission < ActiveRecord::Base
   validates :start_location, presence: true
   validates :end_location, presence: true
 
-  def self.find_by_serial(serial)
+  def self.by_serial(serial)
     Mission
       .joins("INNER JOIN locations AS start_location ON missions.start_location_id = start_location.id")
       .joins("INNER JOIN locations AS end_location ON missions.end_location_id = end_location.id")
       .where("start_location.serial = :serial OR end_location.serial = :serial", serial: serial)
-      .take
+  end
+
+  def self.find_by_serial(serial)
+    by_serial(serial).take
+  end
+
+  def self.find_by_serial!(serial)
+    by_serial(serial).take!
   end
 end
